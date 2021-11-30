3a1b4631861cc3bb95bf8dacec4f92b2a0e17248
gbmiranda1 <gabrielamiranda@alu.ufc.br>
2021-11-30 04:49:24 -0300

    feat: list products

---
+++ src/Pages/initial/style.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    div{
+        margin: 10px;
+    }
+`;
+
+export const DivButtons = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 100%;
+    padding: 30px;
+    box-sizing: border-box;
+
+    button ~ button{
+        margin-left: 10px;
+    }
+`;
+
+
